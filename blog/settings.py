d9ec83cd84cdcf9f9c431996b67e250d3a683bea
Hunter Krieger <hunterk31@hotmail.com>
2018-01-21 17:33:25 -0500

    remove storages'

--- blog/settings.py
+++ blog/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ['vast-cliffs-31137.herokuapp.com',
 INSTALLED_APPS = [
 	'ckeditor',
 	'posts',
-	'storages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
