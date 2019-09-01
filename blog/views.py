0065ec1d0c788721e63c9c2d77cc3bc09345da11
Hunter Krieger <hunterk31@hotmail.com>
2019-09-01 19:33:53 -0400

    add context processor

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render
 from posts.views import Post
 
-five = Post.objects.filter(published=True).order_by("-date_published").all()[:3]
 
 def about(request):
-	context = {'five' : five}
-	return render(request, 'blog/about.html', context)
+	return render(request, 'blog/about.html')
 	
 def contact(request):
-	context = {'five' : five}
-	return render(request, 'blog/contact.html', context)
+	return render(request, 'blog/contact.html')
 	
 def posts(request):
 	post = Post.objects.all()[:5]
-	context = {'post' : post, 'five' : five}
-	return render(request, 'blog/base.html', context)
+	return render(request, 'blog/base.html')
 	
 def listPosts(request):
 	posts = Post.objects.filter(published=True).order_by("-date_published").all()
-	context = {'posts' : posts, 'five' : five}
-	return render(request, 'blog/posts.html', context)
+	context = {'posts' : posts}
+	return render(request, 'blog/posts.html')
--- posts/views.py
+++ posts/views.py
@@ -3,31 +3,18 @@ from django.http import HttpResponse
 from .models import Post, Category
 # Create your views here.
 
-five = Post.objects.filter(published=True).order_by("-date_published").all()[:3]
+
 
 def index(request):
 	posts = Post.objects.filter(published=True).order_by("-date_published").all()[:5]
-	context = {'posts' : posts, 'five' : five}
+	context = {'posts' : posts}
 	return render(request, 'posts/index.html', context)
 
 def read(request, the_slug):
 	post = Post.objects.filter(published=True, slug=the_slug).first()
-	context = {'post' : post, 'five' : five}
+	context = {'post' : post}
 	return render(request, 'posts/single.html', context)
-	
-def category(request, the_category):
-	posts = Post.objects.filter(published=True, category=Category.objects.get(name=the_category)).all()
-	context = {'posts' : posts}
-	return render(request, 'posts/category.html', context)
-	
-def month_archive(request, the_month, the_year):
-	post = Post.objects.filter(date_published__month=the_month).filter(date_published__year=the_year).all()
-	context = {'post' : post}
-	return render(request, 'posts/date.html', context)	
-	
-def year_archive(request, the_year):
-	post = Post.objects.filter(date_published__year=the_year).all()
-	context = {'post' : post}
-	return render(request, 'posts/date.html', context)
+
+
 
 
